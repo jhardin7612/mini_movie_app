bafd10567acb7820c49f1466e007472fc82bbb69
jhardin7612 <118298960+jhardin7612@users.noreply.github.com>
2023-04-03 10:46:31 -0400

    level 0 complete

---
+++ client/go_movies/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+const movies = [
+  {title: 'Mean Girls'},
+  {title: 'Hackers'},
+  {title: 'The Grey'},
+  {title: 'Sunshine'},
+  {title: 'Ex Machina'},
+];
+
+function App() {
+  return (
+    <div className="App">
+      <h3>Movie Titles</h3>
+      {movies.map((movie, index) => <li key={index}>{movie.title}</li>)}
+    </div>
+  );
+}
+
+export default App;
